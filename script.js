2dfdea47c2d1be794382e9614c901d032d229a0d
VismayaMB <152942850+VismayaMB@users.noreply.github.com>
2024-01-26 13:45:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function openSong(songId) {
+    // You can implement logic to navigate to the specific song's page or perform an action
+    console.log(`Opening song with ID ${songId}`);
+    // Add your navigation logic here
+}
+{/* <section id="person1">
+<h2>Person 1's Favorites</h2>
+<div class="song" onclick="openSong('song1')">
+    <img src="cover1.jpg" alt="Song 1 Cover">
+    <audio controls>
+
+        <!-- <source src="horse.ogg" type="audio/ogg"> -->
+        
+        <source src="Harry Potter.mp3" type="audio/mpeg">
+        
+        Your browser does not support the audio element.
+        
+        </audio>
+    <p>Song 1 Title</p>
+</div>
+<div class="song" onclick="openSong('song2')">
+    <img src="cover2.jpg" alt="Song 2 Cover">
+    <p>Song 2 Title</p>
+</div>
+</section>
+
+<section id="person2">
+<h2>Person 2's Favorites</h2>
+</section>
+
+<section id="person3">
+<h2>Person 3's Favorites</h2>
+</section> */}
